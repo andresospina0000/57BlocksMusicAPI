c173d4a13cb38bfe070b66a111f44c8580ed5f1e
Andres Ospina <andresospina0000@gmail.com>
2021-11-28 10:51:34 -0500

    Creating the DbContext (SqlLite)

---
+++ 57Block.Music.Infrastructure/SqlLiteConnection/MusicDbLIteConnection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _57Block.Music.Infrastructure.SqlLiteConnection
+{
+    public class MusicDbLIteConnection : DbContext
+    {
+        public MusicDbLIteConnection() : base()
+        {
+
+        }
+    }
+}
